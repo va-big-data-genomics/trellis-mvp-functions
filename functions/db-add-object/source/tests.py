31fd6b2810c40a70264930b49303591fd0ba3fa6
Paul Billing-Ross <pbilling@stanford.edu>
2019-04-19 12:11:58 -0700

    Can't specify env vars using os

--- functions/db-add-object/source/tests.py
+++ functions/db-add-object/source/tests.py
@@ -3,12 +3,12 @@ import os
 import main as main
 
 # Set environment variables
-os.environ['NEO4J_URL'] = "https://35.247.31.130:7473"
-os.environ['NEO4J_USER'] = "neo4j"
-os.environ['NEO4J_PASSPHRASE'] = "IxH3JD_LNPBQq398xSrPifatw7Ha_SSX"
-os.environ['GOOGLE_CLOUD_PROJECT'] = "gbsc-gcp-project-mvp-dev"
-os.environ['NEW_DB_NODE_TOPIC'] = "trellis-unit-tests"
-os.environ['DATA_GROUP'] = 'wgs-35000'
+#os.environ['NEO4J_URL'] = "https://35.247.31.130:7473"
+#os.environ['NEO4J_USER'] = "neo4j"
+#os.environ['NEO4J_PASSPHRASE'] = "IxH3JD_LNPBQq398xSrPifatw7Ha_SSX"
+#os.environ['GOOGLE_CLOUD_PROJECT'] = "gbsc-gcp-project-mvp-dev"
+#os.environ['NEW_DB_NODE_TOPIC'] = "trellis-unit-tests"
+#os.environ['DATA_GROUP'] = 'wgs-35000'
 
 
 def test_add_fastqc_node():
