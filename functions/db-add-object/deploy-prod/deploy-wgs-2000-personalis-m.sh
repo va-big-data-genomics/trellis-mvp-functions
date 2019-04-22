e341b370dd65714ed899a92890ee93491791360f
Paul Billing-Ross <pbilling@stanford.edu>
2019-04-22 16:55:02 -0700

    Removed manual deploy scripts

--- functions/db-add-object/deploy-prod/deploy-wgs-2000-personalis-m.sh
+++
@@ -1,8 +0,0 @@
-gcloud functions deploy \
-    wgs-2000-add-personalis-obj-to-db-m \
-    --project ***REMOVED*** \
-    --entry-point main \
-    --runtime python37 \
-    --trigger-resource ***REMOVED***-personalis \
-    --trigger-event google.storage.object.metadataUpdate \
-    --env-vars-file wgs-2000-credentials.yaml
