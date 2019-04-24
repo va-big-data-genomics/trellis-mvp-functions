d48176db431b39ffb71477ff9e560fe829a2ff67
Paul Billing-Ross <pbilling@stanford.edu>
2019-04-24 12:05:23 -0700

    Refactor env & input metadata

--- functions/db-query/main.py
+++ functions/db-query/main.py
@@ -5,25 +5,41 @@ import base64
 from py2neo import Graph
 
 from google.cloud import pubsub
+from google.cloud import storage
 
-# Environment variables
-PROJECT_ID = os.environ.get('GOOGLE_CLOUD_PROJECT', '')
-NEO4J_URL = os.environ.get('NEO4J_URL')
-NEO4J_USER = os.environ.get('NEO4J_USER')
-NEO4J_PASSPHRASE = os.environ.get('NEO4J_PASSPHRASE')
+# Get runtime variables from cloud storage bucket
+# https://www.sethvargo.com/secrets-in-serverless/
+ENVIRONMENT = os.environ.get('ENVIRONMENT', '')
+if ENVIRONMENT == 'google-cloud':
+    vars_blob = storage.Client() \
+                .get_bucket(os.environ['CREDENTIALS_BUCKET']) \
+                .get_blob(os.environ['CREDENTIALS_BLOB']) \
+                .download_as_string()
+    parsed_vars = yaml.load(vars_blob, Loader=yaml.Loader)
 
-# Establish PubSub connection
-PUBLISHER = pubsub.PublisherClient()
+    # Runtime variables
+    PROJECT_ID = parsed_vars.get('GOOGLE_CLOUD_PROJECT', '')
+    NEO4J_URL = parsed_vars['NEO4J_URL']
+    NEO4J_USER = parsed_vars['NEO4J_USER']
+    NEO4J_PASSPHRASE = parsed_vars['NEO4J_PASSPHRASE']
+    TOPIC = parsed_vars['DB_QUERY_TOPIC']
+    DATA_GROUP = parsed_vars['DATA_GROUP']
 
-GRAPH = Graph(
-              NEO4J_URL, 
-              user=NEO4J_USER, 
-              password=NEO4J_PASSPHRASE)
+    # Pubsub client
+    PUBLISHER = pubsub.PublisherClient()
+
+    # Neo4j graph
+    GRAPH = Graph(
+                  NEO4J_URL, 
+                  user=NEO4J_USER, 
+                  password=NEO4J_PASSPHRASE)
+
 
 def publish_to_topic(publish_topic, data):
     topic_path = PUBLISHER.topic_path(PROJECT_ID, publish_topic)
     data = json.dumps(data).encode('utf-8')
     PUBLISHER.publish(topic_path, data=data)
+
 
 def query_db(event, context):
     """When an object node is added to the database, launch any
@@ -38,93 +54,73 @@ def query_db(event, context):
     data = json.loads(pubsub_message)
     print(data)
 
-    # Check that resource is request
+    # Check that resource is query
     if data['resource'] != 'query':
         print(f"Error: Expected resource type 'request', " +
               f"got '{data['resource']}.'")
         return
     
+    neo4j_metadata = data['neo4j-metadata']
     trellis_metadata = data['trellis-metadata']
-#    if trellis_metadata['function'] != 'wgs-9000-query-db':
-#        print(f"Info: Skipping; request not for this function.")
-#        return
 
-    query = trellis_metadata['query']
+    query = neo4j_metadata['cypher']
     topic = trellis_metadata.get('publish_topic')
 
-    print(f"Running db query: {query}.")
-    if trellis_metadata['result'] == 'stats':
+    print(f"> Running db query: {query}.")
+    if neo4j_metadata['result'] == 'stats':
         result = GRAPH.run(query).stats()
-        print(f"Result: {result}.")
+        print(f"> Result: {result}.")
 
         if topic:
             publish_to_topic(topic, result)
-    
-    elif trellis_metadata['result'] == 'nodes':
-        nodes = GRAPH.run(query).data()
-        print(f"Nodes count: {len(nodes)}.")
+
+    elif neo4j_metadata['result'] == 'data':
+        result = GRAPH.run(query).data()
+        print(f"> Result count: {len(result)}.")
     
         if topic:
-            print(f"Publishing nodes to {topic}.")
-            for node in nodes:
-                data = {
-                        "resource": "node", 
+            print(f"> Publishing nodes to {topic}.")
+            for datum in result:
+                message = {
+                        "resource": trellis_metadata['result-resource'], 
                         "neo4j-metadata": node
                 }
-                publish_to_topic(topic, data)
+                publish_to_topic(topic, message)
+    else:
+        return RuntimeError(f"! Invalid result type: {trellis_metadata['result']}.")
+    return result
+
 
 if __name__ == "__main__": 
-    PROJECT_ID = "***REMOVED***"
-    NEO4J_URL = "https://35.247.30.111:7473"
+    PROJECT_ID = "***REMOVED***-dev"
+    NEO4J_URL = "https://35.247.31.130:7473"
     NEO4J_USER = "neo4j"
-    NEO4J_PASSPHRASE = "QqkFQ8JHr3-IPReDqu3mXFpalWwpdrgz-UbpD7XifjiDL2XJtadb4zHoOjLta9mNfK2nttoSkbYkPrzbsLa9aA"
+    NEO4J_PASSPHRASE = "IxH3JD_LNPBQq398xSrPifatw7Ha_SSX"
 
     GRAPH = Graph(
-          NEO4J_URL, 
-          user=NEO4J_USER, 
-          password=NEO4J_PASSPHRASE)
+                  NEO4J_URL, 
+                  user=NEO4J_USER, 
+                  password=NEO4J_PASSPHRASE)
 
-    # Delete duplicate nodes
-    """
-    data = {
-            "resource": "request", 
-            "trellis-metadata": {
-                                 "function": "wgs-9000-query-db", 
-                                 "query": "MATCH (n) WITH n.id AS id, COLLECT(n) as nodes WHERE SIZE(nodes) > 1 UNWIND nodes[1..] AS n DETACH DELETE n", 
-                                 "result": "stats"
-            }
-    }
-    data = json.dumps(data).encode('utf-8')
-    event = {'data': base64.b64encode(data)}
-    query_db(event, context=None)
-    """
+    try:
+        # Create blob node
+        data = {
+                "resource": "query", 
+                "neo4j-metadata": {
+                                   "cypher": 'CREATE (node:Fastq:WGS_35000:Blob {bucket: "***REMOVED***-dev-from-personalis", componentCount: 32, contentType: "application/octet-stream", crc32c: "ftNG8w==", etag: "CL3nyPj80uECEBE=", generation: "1555361455813565", id: "***REMOVED***-dev-from-personalis/va_mvp_phase2/***REMOVED***/SHIP4946367/FASTQ/SHIP4946367_0_R1.fastq.gz/1555361455813565", kind: "storage#object", mediaLink: "https://www.googleapis.com/download/storage/v1/b/***REMOVED***-dev-from-personalis/o/va_mvp_phase2%2F***REMOVED***%2FSHIP4946367%2FFASTQ%2FSHIP4946367_0_R1.fastq.gz?generation=1555361455813565&alt=media", metageneration: "17", name: "SHIP4946367_0_R1", selfLink: "https://www.googleapis.com/storage/v1/b/***REMOVED***-dev-from-personalis/o/va_mvp_phase2%2F***REMOVED***%2FSHIP4946367%2FFASTQ%2FSHIP4946367_0_R1.fastq.gz", size: 5955984357, storageClass: "REGIONAL", timeCreated: "2019-04-15T20:50:55.813Z", timeStorageClassUpdated: "2019-04-15T20:50:55.813Z", updated: "2019-04-23T19:17:53.205Z", path: "va_mvp_phase2/***REMOVED***/SHIP4946367/FASTQ/SHIP4946367_0_R1.fastq.gz", dirname: "va_mvp_phase2/***REMOVED***/SHIP4946367/FASTQ", basename: "SHIP4946367_0_R1.fastq.gz", extension: "fastq.gz", timeCreatedEpoch: 1555361455.813, timeUpdatedEpoch: 1556047073.205, timeCreatedIso: "2019-04-15T20:50:55.813000+00:00", timeUpdatedIso: "2019-04-23T19:17:53.205000+00:00", labels: [\'Fastq\', \'WGS_35000\', \'Blob\'], sample: "SHIP4946367", matePair: 1, index: 0}) RETURN node',
+                                   "result": "data",
+                },
+                "trellis-metadata": {"result-resource": "node"}
+        }
+        data = json.dumps(data).encode('utf-8')
+        event = {'data': base64.b64encode(data)}
+        result = query_db(event, context=None)
 
-    # Delete duplicate relationships
-    """
-    data ={
-           "resource": "request", 
-           "trellis-metadata": {
-                                "function": "wgs-9000-query-db", 
-                                "query": "MATCH (s)-[r]-(e) WITH s,e,type(r) as t, collect(r) as rels WHERE SIZE(rels) > 1 UNWIND rels[1..] AS r DELETE r", 
-                                "result": "stats"
-           }
-    }
-    data = json.dumps(data).encode('utf-8')
-    event = {'data': base64.b64encode(data)}
-    query_db(event, context=None)
-    """
-
-    # Get VCFs that need Vcfstats
-    data = {
-            "resource": "request", 
-            "trellis-metadata": {
-                                 "function": "wgs-9000-query-db", 
-                                 "query": "MATCH (node:Vcf) WHERE node.bucket = '***REMOVED***-from-personalis' AND NOT (node)-[:RTG_TOOLS]->(:Vcfstats_data) RETURN node LIMIT 5", 
-                                 "result": "nodes",
-                                 "publish_topic": "wgs-9000-tasks-vcfstats"
-            }
-    }
-    data = json.dumps(data).encode('utf-8')
-    event = {'data': base64.b64encode(data)}
-    query_db(event, context=None)
+        node = result[0]['node']
+        assert len(node.keys()) == 29
+        assert node['path'] == 'va_mvp_phase2/***REMOVED***/SHIP4946367/FASTQ/SHIP4946367_0_R1.fastq.gz'
+        assert node['sample'] == 'SHIP4946367'
+        print("> Blob node creation test: Pass")
+    except:
+        print(f"! Error: blob node did not match expected values. {node}.")
 
