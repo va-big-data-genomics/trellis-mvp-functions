bc4db6e382f45ae15abab81aaa4387df3e6b673f
Paul Billing-Ross <pbilling@stanford.edu>
2019-05-13 14:52:51 -0700

    Added yaml module

--- functions/db-query-index/main.py
+++ functions/db-query-index/main.py
@@ -2,6 +2,7 @@
 import os
 import re 
 import json
+import yaml
 
 from py2neo import Graph
 
--- functions/match-blob-patterns/main.py
+++ functions/match-blob-patterns/main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import yaml
 import importlib
 
 from google.cloud import storage
