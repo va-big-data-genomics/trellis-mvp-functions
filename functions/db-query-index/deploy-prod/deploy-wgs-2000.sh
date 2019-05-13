2be538253f9b6386dfebb5a9c9bb6c66ac3afa53
Paul Billing-Ross <pbilling@stanford.edu>
2019-05-13 13:58:05 -0700

    Removed old build scripts

--- functions/db-query-index/deploy-prod/deploy-wgs-2000.sh
+++
@@ -1,8 +0,0 @@
-gcloud functions deploy \
-    query-db-index-wgs-2000 \
-    --project ***REMOVED*** \
-    --entry-point main \
-    --runtime python37 \
-    --trigger-resource ***REMOVED***-trellis-test \
-    --trigger-event google.storage.object.finalize \
-    --env-vars-file wgs-2000-credentials.yaml 
