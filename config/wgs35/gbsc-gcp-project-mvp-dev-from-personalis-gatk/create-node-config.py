32a5b10c7018decb7ed1609e2704ac4e4eba4f28
Paul Billing-Ross <pbilling@stanford.edu>
2019-05-16 15:26:26 -0700

    formatting

--- config/wgs35/gbsc-gcp-project-mvp-dev-from-personalis-gatk/create-node-config.py
+++ config/wgs35/gbsc-gcp-project-mvp-dev-from-personalis-gatk/create-node-config.py
@@ -10,18 +10,6 @@ from google.cloud import storage
 
 ## Functions for paring custom metadata from blob metadata
 
-#def sample_path_0(db_dict, groupdict):
-#    value = db_dict['path'].split('/')[0] 
-#    return {'sample': str(value)}
-
-#def trellis_workflow_path_1(db_dict, groupdict):
-#    value = db_dict['path'].split('/')[1] 
-#    return {'trellisWorkflow': str(value)}
-
-#def trellis_task_path_2(db_dict, groupdict):
-#    value = db_dict['path'].split('/')[2] 
-#    return {'trellisTask': str(value)}
-
 def trellis_metadata_groupdict(db_dict, groupdict):
     return {
             'sample': groupdict['sample'],
@@ -29,18 +17,22 @@ def trellis_metadata_groupdict(db_dict, groupdict):
             'trellisTask': groupdict['trellis_task']
     }
 
+
 def workflow_path_5(db_dict, groupdict):
     value = db_dict['path'].split('/')[5] 
     return {'workflow': str(value)}
+
 
 def task_path_6(db_dict, groupdict):
     value = db_dict['path'].split('/')[6] 
     task = value.split('-')[1]
     return {'task': str(task)}
 
+
 def shard_index_name_1(db_dict, groupdict):
     index = groupdict['shard_index']
     return {'shardIndex': int(index)}  
+
 
 def get_metadata_from_all_json(db_dict, groupdict):
 
