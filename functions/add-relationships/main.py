3c9a5158edcdddf1331084d50580b51020cc1787
Paul Billing-Ross <pbilling@stanford.edu>
2019-05-28 15:45:22 -0700

    Use header/body structure + unit tests

--- functions/add-relationships/main.py
+++ functions/add-relationships/main.py
@@ -7,6 +7,7 @@ import base64
 from datetime import datetime
 
 from google.cloud import pubsub
+from google.cloud import storage
 
 # Environment variables
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
@@ -19,10 +20,10 @@ if ENVIRONMENT == 'google-cloud':
 
     PROJECT_ID = parsed_vars['GOOGLE_CLOUD_PROJECT']
     TOPIC = parsed_vars['DB_QUERY_TOPIC']
+    DATA_GROUP = parsed_vars['DATA_GROUP']
 
     # Establish PubSub connection
     PUBLISHER = pubsub.PublisherClient()
-    TOPIC_PATH = f"projects/{PROJECT_ID}/topics/{TOPIC}"
 
 
 def publish_message(topic, query):
@@ -32,6 +33,12 @@ def publish_message(topic, query):
                "trellis": {"sent-from": "add-relationships"},
     }
     publish_to_topic(topic, message)
+    print(f"> Published following message to {topic}: {message}.")
+
+def publish_to_topic(topic, data):
+    topic_path = PUBLISHER.topic_path(PROJECT_ID, topic)
+    message = json.dumps(data).encode('utf-8')
+    PUBLISHER.publish(topic_path, data=message)
     print(f"> Published following message to {topic}: {message}.")
 
 
@@ -48,23 +55,25 @@ def add_relationships(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     data = json.loads(pubsub_message)
     print(f"Data: {data}.\n")
+    header = data['header']
+    body = data['body']
 
-    if data['resource'] != 'query-result':
+    if header['resource'] != 'query-result':
         print(f"Error: Expected resource type 'blob', " +
               f"got '{data['resource']}.'")
         return
 
-    node = data['results']
-    relationships = data['relationships']
+    node = body['results'][0]['node']
+    relationships = body['relationships']
     
     # Get node labels
-    labels = node['labels']
-    print(f"Node labels: {labels}.\n")
+    #labels = node['labels']
+    #print(f"Node labels: {labels}.\n")
 
     # Write a generic relationship query
     for orientation in relationships:
-        for relationship_name in orientation:
-            for related_node in relationship_name:
+        for relationship_name in relationships[orientation]:
+            for related_node in relationships[orientation][relationship_name]:
                 
                 # Create property string for related node
                 related_strings = []
@@ -86,36 +95,49 @@ def add_relationships(event, context):
                 
                 if orientation == "to-node":
                     query = f"""
-                            MATCH (related_node {related_string}), 
-                                  (node {node_string})
+                            MATCH (related_node {{ {related_string} }}), 
+                                  (node {{ {node_string} }})
                             CREATE (related_node)-[:{relationship_name}]->(node)
                             """
                 elif orientation == "from-node":
                     query = f"""
-                            MATCH (related_node {related_string}), 
-                                  (node {node_string})
+                            MATCH (related_node {{ {related_string} }}), 
+                                  (node {{ {node_string} }})
                             CREATE (node)-[:{relationship_name}]->(related_node)
                             """
                 elif orientation == "bidirectional":
                     query = f"""
-                            MATCH (related_node {related_string}), 
-                                  (node {node_string})
+                            MATCH (related_node {{ {related_string} }}), 
+                                  (node {{ {node_string} }})
                             CREATE (node)-[:{relationship_name}]-(related_node)
                             """
                 # Compose query
                 # CREATE (node)-[:{relationship_name}]->(related_node {related_node})
-                publish_message(topic, query)
+
+                message = {
+                           "header": {
+                                      "resource": "query", 
+                                      "method": "POST",
+                                      "labels": ["Cypher", "Query", "Relationship", "Create"],
+                           },
+                           "body": {
+                                    "cypher": query,
+                                    "sent-from": "{DATA_GROUP}-add-relationships",
+                           }
+                }
+                publish_to_topic(TOPIC, message)
 
 
 # For local testing
 if __name__ == "__main__":
     PROJECT_ID = "***REMOVED***-dev"
+    TOPIC = "wgs35-db-queries"
     TOPIC_PATH = f"projects/{PROJECT_ID}/topics/{TOPIC}"
 
-    data = {'resource': 'node', 'neo4j-metadata': {'node': {'extension': 'snvindel.var.vcf.gz', 'time-created-iso': '2019-03-11T23:08:55.327000+00:00', 'dirname': 'SHIP3935743/Variants', 'path': 'SHIP3935743/Variants/SHIP3935743.snvindel.var.vcf.gz', 'storageClass': 'REGIONAL', 'md5Hash': 'a7zCh6W1CLUca9JbkWBrmg==', 'timeCreated': '2019-03-11T23:08:55.327Z', 'time-updated-iso': '2019-03-26T23:23:02.512000+00:00', 'id': '***REMOVED***-dev-from-personalis/SHIP3935743/Variants/SHIP3935743.snvindel.var.vcf.gz/1552345735327945', 'contentType': 'text/vcard', 'generation': '1552345735327945', 'metageneration': '15', 'kind': 'storage#object', 'sample': 'SHIP3935743', 'mediaLink': 'https://www.googleapis.com/download/storage/v1/b/***REMOVED***-dev-from-personalis/o/SHIP3935743%2FVariants%2FSHIP3935743.snvindel.var.vcf.gz?generation=1552345735327945&alt=media', 'selfLink': 'https://www.googleapis.com/storage/v1/b/***REMOVED***-dev-from-personalis/o/SHIP3935743%2FVariants%2FSHIP3935743.snvindel.var.vcf.gz', 'labels': ['Vcf', 'WGS_9000', 'Blob'], 'bucket': '***REMOVED***-dev-from-personalis', 'basename': 'SHIP3935743.snvindel.var.vcf.gz', 'crc32c': 'EMJeaA==', 'size': 224959007, 'timeStorageClassUpdated': '2019-03-11T23:08:55.327Z', 'name': 'SHIP3935743', 'etag': 'CMnh/cCa++ACEA8=', 'updated': '2019-03-26T23:23:02.512Z', 'time-created-epoch': 1552345735.327, 'time-updated-epoch': 1553642582.512}}}
-    data = json.dumps(data)
-    data = data.encode('utf-8')
-    
+    PUBLISHER = pubsub.PublisherClient()
+
+    data = b'{"header": {"method": "VIEW", "resource": "query-result", "labels": ["Cypher", "Query", "Result"]}, "body": {"query": "CREATE (node:Job:Cromwell {provider: \\"google-v2\\", user: \\"trellis\\", zones: \\"us-west1*\\", project: \\"***REMOVED***-dev\\", minCores: 1, minRam: 6.5, preemptible: True, bootDiskSize: 20, image: \\"gcr.io/***REMOVED***-dev/***REMOVED***/wdl_runner:latest\\", logging: \\"gs://***REMOVED***-dev-from-personalis-gatk-logs/SHIP4946367/fastq-to-vcf/gatk-5-dollar/logs\\", diskSize: 1000, command: \\"java -Dconfig.file=${CFG} -Dbackend.providers.JES.config.project=${MYproject} -Dbackend.providers.JES.config.root=${ROOT} -jar /cromwell/cromwell.jar run ${WDL} --inputs ${INPUT} --options ${OPTION}\\", dryRun: True, labels: [\'Job\', \'Cromwell\'], input_CFG: \\"gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/google-adc.conf\\", input_OPTION: \\"gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/generic.google-papi.options.json\\", input_WDL: \\"gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/fc_germline_single_sample_workflow.wdl\\", input_SUBWDL: \\"gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/tasks_pipelines/*.wdl\\", input_INPUT: \\"gs://***REMOVED***-dev-from-personalis-gatk/SHIP4946367/fastq-to-vcf/gatk-5-dollar/inputs/inputs.json\\", env_MYproject: \\"***REMOVED***-dev\\", env_ROOT: \\"gs://***REMOVED***-dev-from-personalis-gatk/SHIP4946367/fastq-to-vcf/gatk-5-dollar/output\\", timeCreatedEpoch: 1559080699.59893, timeCreatedIso: \\"2019-05-28T21:58:19.598930+00:00\\"}) RETURN node", "sent-from": "wgs35-db-query", "relationships": {"to-node": {"INPUT_TO": [{"basename": "SHIP4946367_2.ubam", "bucket": "***REMOVED***-dev-from-personalis-gatk", "contentType": "application/octet-stream", "crc32c": "ojStVg==", "dirname": "SHIP4946367/fastq-to-vcf/fastq-to-ubam/output", "etag": "CJTpxe3ynuICEAM=", "extension": "ubam", "generation": "1557970088457364", "id": "***REMOVED***-dev-from-personalis-gatk/SHIP4946367/fastq-to-vcf/fastq-to-ubam/output/SHIP4946367_2.ubam/1557970088457364", "kind": "storage#object", "labels": ["WGS35", "Blob", "Ubam"], "md5Hash": "opGAi0f9olAu4DKzvYiayg==", "mediaLink": "https://www.googleapis.com/download/storage/v1/b/***REMOVED***-dev-from-personalis-gatk/o/SHIP4946367%2Ffastq-to-vcf%2Ffastq-to-ubam%2Foutput%2FSHIP4946367_2.ubam?generation=1557970088457364&alt=media", "metageneration": "3", "name": "SHIP4946367_2", "path": "SHIP4946367/fastq-to-vcf/fastq-to-ubam/output/SHIP4946367_2.ubam", "sample": "SHIP4946367", "selfLink": "https://www.googleapis.com/storage/v1/b/***REMOVED***-dev-from-personalis-gatk/o/SHIP4946367%2Ffastq-to-vcf%2Ffastq-to-ubam%2Foutput%2FSHIP4946367_2.ubam", "size": 16886179620, "storageClass": "REGIONAL", "timeCreated": "2019-05-16T01:28:08.455Z", "timeCreatedEpoch": 1557970088.455, "timeCreatedIso": "2019-05-16T01:28:08.455000+00:00", "timeStorageClassUpdated": "2019-05-16T01:28:08.455Z", "timeUpdatedEpoch": 1558045261.522, "timeUpdatedIso": "2019-05-16T22:21:01.522000+00:00", "trellisTask": "fastq-to-ubam", "trellisWorkflow": "fastq-to-vcf", "updated": "2019-05-16T22:21:01.522Z"}, {"basename": "SHIP4946367_0.ubam", "bucket": "***REMOVED***-dev-from-personalis-gatk", "contentType": "application/octet-stream", "crc32c": "ZaJM+g==", "dirname": "SHIP4946367/fastq-to-vcf/fastq-to-ubam/output", "etag": "CM+sxKDynuICEAY=", "extension": "ubam", "generation": "1557969926952527", "id": "***REMOVED***-dev-from-personalis-gatk/SHIP4946367/fastq-to-vcf/fastq-to-ubam/output/SHIP4946367_0.ubam/1557969926952527", "kind": "storage#object", "labels": ["WGS35", "Blob", "Ubam"], "md5Hash": "Tgh+eyIiKe8TRWV6vohGJQ==", "mediaLink": "https://www.googleapis.com/download/storage/v1/b/***REMOVED***-dev-from-personalis-gatk/o/SHIP4946367%2Ffastq-to-vcf%2Ffastq-to-ubam%2Foutput%2FSHIP4946367_0.ubam?generation=1557969926952527&alt=media", "metageneration": "6", "name": "SHIP4946367_0", "path": "SHIP4946367/fastq-to-vcf/fastq-to-ubam/output/SHIP4946367_0.ubam", "sample": "SHIP4946367", "selfLink": "https://www.googleapis.com/storage/v1/b/***REMOVED***-dev-from-personalis-gatk/o/SHIP4946367%2Ffastq-to-vcf%2Ffastq-to-ubam%2Foutput%2FSHIP4946367_0.ubam", "size": 16871102587, "storageClass": "REGIONAL", "timeCreated": "2019-05-16T01:25:26.952Z", "timeCreatedEpoch": 1557969926.952, "timeCreatedIso": "2019-05-16T01:25:26.952000+00:00", "timeStorageClassUpdated": "2019-05-16T01:25:26.952Z", "timeUpdatedEpoch": 1558045265.901, "timeUpdatedIso": "2019-05-16T22:21:05.901000+00:00", "trellisTask": "fastq-to-ubam", "trellisWorkflow": "fastq-to-vcf", "updated": "2019-05-16T22:21:05.901Z"}]}}, "results": [{"node": {"image": "gcr.io/***REMOVED***-dev/***REMOVED***/wdl_runner:latest", "dryRun": true, "minCores": 1, "input_SUBWDL": "gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/tasks_pipelines/*.wdl", "input_WDL": "gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/fc_germline_single_sample_workflow.wdl", "project": "***REMOVED***-dev", "zones": "us-west1*", "input_CFG": "gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/google-adc.conf", "command": "java -Dconfig.file=${CFG} -Dbackend.providers.JES.config.project=${MYproject} -Dbackend.providers.JES.config.root=${ROOT} -jar /cromwell/cromwell.jar run ${WDL} --inputs ${INPUT} --options ${OPTION}", "labels": ["Job", "Cromwell"], "diskSize": 1000, "preemptible": true, "provider": "google-v2", "input_OPTION": "gs://***REMOVED***-dev-trellis/workflow-inputs/gatk-mvp/gatk-mvp-pipeline/generic.google-papi.options.json", "env_ROOT": "gs://***REMOVED***-dev-from-personalis-gatk/SHIP4946367/fastq-to-vcf/gatk-5-dollar/output", "timeCreatedEpoch": 1559080699.59893, "minRam": 6.5, "logging": "gs://***REMOVED***-dev-from-personalis-gatk-logs/SHIP4946367/fastq-to-vcf/gatk-5-dollar/logs", "timeCreatedIso": "2019-05-28T21:58:19.598930+00:00", "env_MYproject": "***REMOVED***-dev", "input_INPUT": "gs://***REMOVED***-dev-from-personalis-gatk/SHIP4946367/fastq-to-vcf/gatk-5-dollar/inputs/inputs.json", "bootDiskSize": 20, "user": "trellis"}}]}}'
+    #data = json.dumps(data).encode('utf-8')
     event = {'data': base64.b64encode(data)}
 
     add_relationships(event, context=None)
