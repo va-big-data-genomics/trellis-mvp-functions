28dff955dad951454c9a5db55b258618e4df74b2
Paul Billing-Ross <pbilling@stanford.edu>
2020-04-02 16:47:59 -0700

    Added google-beta provider & zone/region info

--- terraform/trellis/main.tf
+++ terraform/trellis/main.tf
@@ -6,16 +6,18 @@
 |
 */
 
-// Variables
-variable "project" {
-    type = string
-}
-
 provider "google" {
   project = var.project
   region  = "us-west1"
   zone    = "us-west1-b"
 }
+
+provider "google-beta" {
+  project = var.project
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
 
 resource "google_compute_project_metadata" "default" {
     metadata = {
