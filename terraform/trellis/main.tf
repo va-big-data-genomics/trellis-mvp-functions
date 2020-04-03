bf8667a97196102cdd66d142811c3b05831313d4
Paul Billing-Ross <pbilling@stanford.edu>
2020-04-03 16:55:16 -0700

    Add disk snapshot schedule

--- terraform/trellis/main.tf
+++ terraform/trellis/main.tf
@@ -25,3 +25,16 @@ resource "google_compute_project_metadata" "default" {
         trellis-subnetwork = "us-west1"
     }
 }
+
+resource "google_compute_resource_policy" "daily_snapshot" {
+  name   = "dailysnapshot"
+  region = "us-west1"
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time    = "04:00"
+      }
+    }
+  }
+}
