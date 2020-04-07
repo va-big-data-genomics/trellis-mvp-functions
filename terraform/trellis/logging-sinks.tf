af30ea50276c71210da83c040dbb5373412155af
Paul Billing-Ross <pbilling@stanford.edu>
2020-04-07 11:38:43 -0700

    Use modules to deploy logging sinks & topics

--- terraform/trellis/logging-sinks.tf
+++ terraform/trellis/logging-sinks.tf
@@ -7,41 +7,35 @@
 |
 */
 
-/* Don't have permission 
-resource "google_project_iam_binding" "log-writer" {
-    role = "roles/pubsub.publisher"
-
-    members = [
-        "serviceAccount:cloud-logs@system.gserviceaccount.com",
-    ]
-}
-*/
-
-resource "google_logging_project_sink" "sink-delete-pipelines-worker" {
-    name = "sink-delete-pipelines-worker"
-
-    # Can export to pubsub, cloud storage, or bigquery
-    destination = "pubsub.googleapis.com/projects/${var.project}/topics/${google_pubsub_topic.log-delete-instance.name}"
-
-    filter = <<EOT
+module "delete_instance_log" {
+  source                 = "./google-log-export"
+  destination_uri        = module.delete_instance_topic.destination_uri
+  filter                 = <<EOT
 resource.type = gce_instance AND 
 protoPayload.serviceName = compute.googleapis.com 
 AND protoPayload.request.@type = type.googleapis.com/compute.instances.delete 
 AND protoPayload.resourceName:google-pipelines-worker
 EOT
-
-    unique_writer_identity = true
-
-
+  log_sink_name          = "delete-pipelines-worker"
+  // What is this?
+  #parent_resource_id     = var.parent_resource_id
+  parent_resource_id     = var.project
+  parent_resource_type   = "project"
+  unique_writer_identity = true
 }
 
-resource "google_logging_project_sink" "sink-insert-cromwell-instance" {
-    name = "sink-insert-cromwell-instance"
+module "delete_instance_topic" {
+  source                   = "./pubsub"
+  project_id               = var.project
+  topic_name               = "delete-pipelines-worker"
+  log_sink_writer_identity = module.delete_instance_log.writer_identity
+  create_subscriber        = false
+}
 
-    # Can export to pubsub, cloud storage, or bigquery
-    destination = "pubsub.googleapis.com/projects/${var.project}/topics/${google_pubsub_topic.log-insert-cromwell-instance.name}"
-
-    filter = <<EOT
+module "insert_cromwell_log" {
+  source                 = "./google-log-export"
+  destination_uri        = module.insert_cromwell_topic.destination_uri
+  filter                 = <<EOT
 resource.type = gce_instance 
 AND protoPayload.serviceName = compute.googleapis.com 
 AND protoPayload.request.@type = type.googleapis.com/compute.instances.insert
@@ -49,18 +43,24 @@ AND protoPayload.request.networkInterfaces.network: networks/${google_compute_ne
 AND protoPayload.resourceName:google-pipelines-worker
 AND protoPayload.request.labels.key:cromwell-workflow-id
 EOT
-
-    unique_writer_identity = true
-
+  log_sink_name          = "insert-cromwell-instance"
+  parent_resource_id     = var.project
+  parent_resource_type   = "project"
+  unique_writer_identity = true
 }
 
-resource "google_logging_project_sink" "sink-insert-trellis-instance" {
-    name = "sink-insert-trellis-instance"
+module "insert_cromwell_topic" {
+  source                   = "./pubsub"
+  project_id               = var.project
+  topic_name               = "insert-cromwell-instance"
+  log_sink_writer_identity = module.insert_cromwell_log.writer_identity
+  create_subscriber        = false
+}
 
-    # Can export to pubsub, cloud storage, or bigquery
-    destination = "pubsub.googleapis.com/projects/${var.project}/topics/${google_pubsub_topic.log-insert-cromwell-instance.name}"
-
-    filter = <<EOT
+module "insert_trellis_log" {
+  source                 = "./google-log-export"
+  destination_uri        = module.insert_trellis_topic.destination_uri
+  filter                 = <<EOT
 resource.type = gce_instance 
 AND protoPayload.serviceName = compute.googleapis.com 
 AND protoPayload.request.@type = type.googleapis.com/compute.instances.insert
@@ -68,7 +68,16 @@ AND protoPayload.request.networkInterfaces.network: networks/${google_compute_ne
 AND protoPayload.resourceName:google-pipelines-worker
 AND protoPayload.request.labels.key:trellis-id
 EOT
+  log_sink_name          = "insert-trellis-instance"
+  parent_resource_id     = var.project
+  parent_resource_type   = "project"
+  unique_writer_identity = true
+}
 
-    unique_writer_identity = true
-
+module "insert_trellis_topic" {
+  source                   = "./pubsub"
+  project_id               = var.project
+  topic_name               = "insert-trellis-instance"
+  log_sink_writer_identity = module.insert_trellis_log.writer_identity
+  create_subscriber        = false
 }
