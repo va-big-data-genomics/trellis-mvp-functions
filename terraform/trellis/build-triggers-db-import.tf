0a5019280fbb12289a1c88547e1bb873883f8d9d
Paul Billing-Ross <pbilling@stanford.edu>
2020-04-07 16:50:27 -0700

    Add dynamic references to config object

--- terraform/trellis/build-triggers-db-import.tf
+++ terraform/trellis/build-triggers-db-import.tf
@@ -5,10 +5,11 @@
 |
 | Deploy cloud functions for enabling request-driven database import.
 |
-*
+*/
 
 resource "google_cloudbuild_trigger" "list-bucket-page" {
-    provider = google-beta
+    provider    = google-beta
+    name        = "gcf-list-bucket-page"
     
     github {
         owner = var.github-owner
@@ -23,15 +24,16 @@ resource "google_cloudbuild_trigger" "list-bucket-page" {
     filename = "functions/list-bucket-page/cloudbuild.yaml"
 
     substitutions = {
-        _CREDENTIALS_BLOB   = "credentials/trellis.yaml"
-        _CREDENTIALS_BUCKET = "${var.project}-trellis"
+        _CREDENTIALS_BLOB   = google_storage_bucket_object.trellis-config.name
+        _CREDENTIALS_BUCKET = google_storage_bucket.trellis.name
         _ENVIRONMENT        = "google-cloud"
         _TRIGGER_TOPIC      = google_pubsub_topic.list-bucket-page.name
     }
 }
 
 resource "google_cloudbuild_trigger" "match-blob-patterns" {
-    provider = google-beta
+    provider    = google-beta
+    name        = "gcf-match-blob-patterns"
     
     github {
         owner = var.github-owner
@@ -46,16 +48,17 @@ resource "google_cloudbuild_trigger" "match-blob-patterns" {
     filename = "functions/match-blob-patterns/cloudbuild.yaml"
 
     substitutions = {
-        _CREDENTIALS_BLOB   = "credentials/trellis.yaml"
-        _CREDENTIALS_BUCKET = "${var.project}-trellis"
+        _CREDENTIALS_BLOB   = google_storage_bucket_object.trellis-config.name
+        _CREDENTIALS_BUCKET = google_storage_bucket.trellis.name
         _ENVIRONMENT        = "google-cloud"
         _TRIGGER_TOPIC      = google_pubsub_topic.match-blob-patterns.name
-        _DATA_GROUP         = "phase3"
+        _DATA_GROUP         = "${var.data-group}"
     }
 }
 
 resource "google_cloudbuild_trigger" "db-query-index" {
-    provider = google-beta
+    provider    = google-beta
+    name        = "gcf-db-query-index"
     
     github {
         owner = var.github-owner
@@ -70,15 +73,16 @@ resource "google_cloudbuild_trigger" "db-query-index" {
     filename = "functions/db-query-index/cloudbuild.yaml"
 
     substitutions = {
-        _CREDENTIALS_BLOB   = "credentials/trellis.yaml"
-        _CREDENTIALS_BUCKET = "${var.project}-trellis"
+        _CREDENTIALS_BLOB   = google_storage_bucket_object.trellis-config.name
+        _CREDENTIALS_BUCKET = google_storage_bucket.trellis.name
         _ENVIRONMENT        = "google-cloud"
         _TRIGGER_TOPIC      = google_pubsub_topic.db-query-index.name
     }
 }
 
 resource "google_cloudbuild_trigger" "update-metadata" {
-    provider = google-beta
+    provider    = google-beta
+    name        = "gcf-update-metadata"
     
     github {
         owner = var.github-owner
@@ -93,8 +97,8 @@ resource "google_cloudbuild_trigger" "update-metadata" {
     filename = "functions/update-metadata/cloudbuild.yaml"
 
     substitutions = {
-        _CREDENTIALS_BLOB   = "credentials/trellis.yaml"
-        _CREDENTIALS_BUCKET = "${var.project}-trellis"
+        _CREDENTIALS_BLOB   = google_storage_bucket_object.trellis-config.name
+        _CREDENTIALS_BUCKET = google_storage_bucket.trellis.name
         _ENVIRONMENT        = "google-cloud"
         _TRIGGER_TOPIC      = google_pubsub_topic.update-metadata.name
     }
