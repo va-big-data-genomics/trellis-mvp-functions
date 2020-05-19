e08a5a8d1578783f36cf51ce764730157200c575
Paul Billing-Ross <pbilling@stanford.edu>
2020-05-19 12:45:03 -0700

    debugging

--- functions/postgres-insert-data/main.py
+++ functions/postgres-insert-data/main.py
@@ -275,12 +275,12 @@ def postgres_insert_data(event, context):
     info.logging(f"Table name: {table_name}.")
     info.logging(f"Schema fields: {schema_fields}.")
 
+    # Debugging
+    return
+
     # TODO: this is broken
     # Check whether table exists
     table_exists = table_exists(DB_CONN, table_name)
-
-    # Debugging
-    return
 
     if not table_exists:
         # If not, create table
