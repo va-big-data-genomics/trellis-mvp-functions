90bcfe4115d69f7e42f428e95a7c4a325b9ef195
Paul Billing-Ross <pbilling@stanford.edu>
2020-06-08 18:01:27 -0700

    LaunchFlagstat: Add Blob label to query to use index

--- config/phase3/database-triggers.py
+++ config/phase3/database-triggers.py
@@ -1249,7 +1249,7 @@ class LaunchFlagstat:
 
     def _create_query(self, blob_id, event_id):
         query = (
-                 f"MATCH (s:CromwellStep)-[:OUTPUT]->(node:Bam) " +
+                 f"MATCH (s:CromwellStep)-[:OUTPUT]->(node:Blob:Bam) " +
                  "WHERE s.wdlCallAlias=\"gatherbamfiles\" " +
                  f"AND node.id =\"{blob_id}\" " +
                  "AND NOT (node)-[:INPUT_TO]->(:JobRequest:Flagstat) " +
