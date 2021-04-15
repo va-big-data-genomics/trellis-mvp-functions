5854ea794bd4294409a7f358d3d3ba93a364db56
Paul Billing-Ross <pbilling@stanford.edu>
2021-04-15 12:18:02 -0700

    Check if private IPs work

--- functions/launch-view-gvcf-snps/main.py
+++ functions/launch-view-gvcf-snps/main.py
@@ -290,7 +290,7 @@ def launch_view_gvcf_snps(event, context, test=False):
         "--min-cores", str(job_dict["minCores"]), 
         "--logging", job_dict["logging"],
         "--image", job_dict["image"],
-        #"--use-private-address",
+        "--use-private-address",
         #"--block-external-network",
         "--ssh",
         "--network", job_dict["network"],
