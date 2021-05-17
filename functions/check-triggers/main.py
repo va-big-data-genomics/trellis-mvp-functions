e70680cc13761e662dbba8774823a3a9e23bf5d4
Paul Billing-Ross <pbilling@stanford.edu>
2021-05-17 16:13:00 -0700

    Changing log level for listing triggers

--- functions/check-triggers/main.py
+++ functions/check-triggers/main.py
@@ -72,7 +72,7 @@ def check_triggers(event, context, dry_run=False):
     activated_triggers = []
     for trigger in ALL_TRIGGERS:
         #status = trigger.check_conditions(node)
-        logging.debug(f"> Checking trigger: {trigger}.")
+        logging.info(f"> Checking trigger: {trigger}.")
         status = trigger.check_conditions(header, body, node)
         if status == True:
             activated_triggers.append(trigger)
