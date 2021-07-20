f57f1605fb8313018e3bfae8d36613956cd782b0
Paul Billing-Ross <pbilling@stanford.edu>
2021-07-20 13:29:14 -0700

    Dynamically choose reference fasta bucket

--- functions/launch-cnvnator/main.py
+++ functions/launch-cnvnator/main.py
@@ -209,7 +209,7 @@ def launch_cnvnator(event, context, test=False):
         "inputs": {
             "CRAM": f"gs://{cram['bucket']}/{cram['path']}",
         },
-        "inputRecursive": "DIR=gs://gbsc-gcp-project-mvp-genomics-public-data/references/GRCh38/unzipped",
+        "inputRecursive": f"DIR=gs://{TRELLIS.GOOGLE_CLOUD_PROJECT}-genomics-public-data/references/GRCh38/unzipped",
         "outputs": {
             "ROOT": f"gs://{TRELLIS.DSUB_OUT_BUCKET}/{plate}/{sample}/{task_name}/{task_id}/output/{sample}.root",
             "CALL_OUT": f"gs://{TRELLIS.DSUB_OUT_BUCKET}/{plate}/{sample}/{task_name}/{task_id}/output/{sample}.{sample}.out",
