f5407b29ab2f559a45be3479117e1618ef799b64
Paul Billing-Ross <pbilling@stanford.edu>
2021-07-22 17:04:26 -0700

    Specfify bin size using environment variable

--- functions/launch-cnvnator/CNVnator.sh
+++ functions/launch-cnvnator/CNVnator.sh
@@ -2,10 +2,10 @@
 
 #!/bin/bash 
   cnvnator -unique -root $ROOT -tree $BAM -chrom $(seq -f 'chr%g' 1 22) chrX chrY chrM 
-  cnvnator -root $ROOT -his 100 -d $DIR 
-  cnvnator -root $ROOT -stat 100 
-  cnvnator -root $ROOT -eval 100 > $EVAL_OUT 
-  cnvnator -root $ROOT -partition 100 
-  cnvnator -root $ROOT -call 100 > $CALL_OUT 
+  cnvnator -root $ROOT -his ${BIN_SIZE} -d $DIR 
+  cnvnator -root $ROOT -stat ${BIN_SIZE} 
+  cnvnator -root $ROOT -eval ${BIN_SIZE} > $EVAL_OUT 
+  cnvnator -root $ROOT -partition ${BIN_SIZE} 
+  cnvnator -root $ROOT -call ${BIN_SIZE} > $CALL_OUT 
   perl /app/CNVnator_v0.4.1/src/cnvnator2VCF.pl $CALL_OUT $DIR > $CALL_VCF 
-  awk '{print $2} END {print "exit"}' $CALL_OUT | cnvnator -root $ROOT -genotype 100 > $GENOTYPE_OUT
+  awk '{print $2} END {print "exit"}' $CALL_OUT | cnvnator -root $ROOT -genotype ${BIN_SIZE} > $GENOTYPE_OUT
