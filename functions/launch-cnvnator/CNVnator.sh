129aa69f9ae6e9f18fd5b5952cd8ef46e590a82f
Paul Billing-Ross <pbilling@stanford.edu>
2021-07-28 10:36:41 -0700

    Providing reference sequence cache to decode CRAMs

--- functions/launch-cnvnator/CNVnator.sh
+++ functions/launch-cnvnator/CNVnator.sh
@@ -1,11 +1,16 @@
+#!/bin/bash 
 #CNVnator BASH Script
 
-#!/bin/bash 
-  cnvnator -unique -root $ROOT -tree $BAM -chrom $(seq -f 'chr%g' 1 22) chrX chrY chrM 
-  cnvnator -root $ROOT -his ${BIN_SIZE} -d $DIR 
-  cnvnator -root $ROOT -stat ${BIN_SIZE} 
-  cnvnator -root $ROOT -eval ${BIN_SIZE} > $EVAL_OUT 
-  cnvnator -root $ROOT -partition ${BIN_SIZE} 
-  cnvnator -root $ROOT -call ${BIN_SIZE} > $CALL_OUT 
-  perl /app/CNVnator_v0.4.1/src/cnvnator2VCF.pl $CALL_OUT $DIR > $CALL_VCF 
-  awk '{print $2} END {print "exit"}' $CALL_OUT | cnvnator -root $ROOT -genotype ${BIN_SIZE} > $GENOTYPE_OUT
+# https://github.com/DecodeGenetics/graphtyper/issues/57
+tar xzf ${REF_CACHE_SOURCE}
+export REF_PATH="$(pwd)/ref/cache/%2s/%2s/%s:http://www.ebi.ac.uk/ena/cram/md5/%s"
+export REF_CACHE="$(pwd)/ref/cache/%2s/%2s/%s"
+
+cnvnator -unique -root ${ROOT} -tree ${BAM} -chrom $(seq -f 'chr%g' 1 22) chrX chrY chrM 
+cnvnator -root ${ROOT} -his ${BIN_SIZE} -d ${DIR} 
+cnvnator -root ${ROOT} -stat ${BIN_SIZE} 
+cnvnator -root ${ROOT} -eval ${BIN_SIZE} > ${EVAL_OUT} 
+cnvnator -root ${ROOT} -partition ${BIN_SIZE} 
+cnvnator -root ${ROOT} -call ${BIN_SIZE} > ${CALL_OUT} 
+perl /app/CNVnator_v0.4.1/src/cnvnator2VCF.pl ${CALL_OUT} ${DIR} > ${CALL_VCF} 
+awk '{print $2} END {print "exit"}' ${CALL_OUT} | cnvnator -root ${ROOT} -genotype ${BIN_SIZE} > ${GENOTYPE_OUT}
