e4def844e6c74f4fd2ae853d7a422828c1aa5870
Paul Billing-Ross <pbilling@stanford.edu>
2022-03-24 17:18:16 -0700

    Import TaxonomyParser class from trellisdata

--- functions/create-blob-node/main.py
+++ functions/create-blob-node/main.py
@@ -16,125 +16,6 @@ from collections import deque
 from google.cloud import storage
 from google.cloud import pubsub
 
-class Struct:
-    # https://stackoverflow.com/questions/6866600/how-to-parse-read-a-yaml-file-into-a-python-object
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-
-
-class TaxonomyParser:
-    """
-    This class is a wrapper on a Tree class from the anytree library to hold
-    different data hierarchies read from a JSON representation
-
-    Source: https://towardsdatascience.com/represent-hierarchical-data-in-python-cd36ada5c71a
-    """
-
-    def __init__(self, level_prefix = "L"):
-        self.prefix = level_prefix
-        self.nodes = {}
-        self.root_key = None
-
-    def find_by_name(self, name) -> Node:
-        """
-        Retrieve a node by its unique identifier name
-        """
-        root = self.nodes[self.root_key]
-        node = find(root, lambda node: node.name == name)
-        return node
-   
-    def read_from_json(self, fname):
-        """
-        Read the taxonomy from a JSON file given as input
-        """
-        
-        self.nodes = {}
-        try:
-            with open(fname, "r") as f:
-                data = json.load(f)
-                n_levels = len(list(data.keys()))
-
-                # read the root node
-                root = data[f"{self.prefix}0"][0]
-                name = root["name"]
-                _ = root.pop("name")
-                
-                self.nodes[name] = Node(name, **root)
-                self.root_key = name
-
-                # populate the tree
-                for k in range(1, n_levels):
-                    
-                    key = f"{self.prefix}{k}"
-                    nodes = data[key]
-
-                    for n in nodes:
-                        try:
-                            assert "name" in n
-                            name = n["name"]
-                            _ = n.pop("name")
-                            parent = n["parent"]
-                            _ = n.pop("parent")
-                            
-                            self.nodes[name] = Node(
-                                name,
-                                parent=self.nodes[parent],
-                                **n
-                            )
-                        except AssertionError:
-                            print(f"Malformed node representation: {n}")
-                        except KeyError:
-                            print(f"Detected a dangling node: {n['name']}")
-
-        except (FileNotFoundError, KeyError):
-            raise Exception("Not existent or malformed input JSON file")
-
-    def read_from_string(self, data_str):
-        """
-        Read the taxonomy from a JSON file given as input
-        """
-        
-        self.nodes = {}
-        try:
-            data = json.loads(data_str)
-            n_levels = len(list(data.keys()))
-
-            # read the root node
-            root = data[f"{self.prefix}0"][0]
-            name = root["name"]
-            _ = root.pop("name")
-            
-            self.nodes[name] = Node(name, **root)
-            self.root_key = name
-
-            # populate the tree
-            for k in range(1, n_levels):
-                
-                key = f"{self.prefix}{k}"
-                nodes = data[key]
-
-                for n in nodes:
-                    try:
-                        assert "name" in n
-                        name = n["name"]
-                        _ = n.pop("name")
-                        parent = n["parent"]
-                        _ = n.pop("parent")
-                        
-                        self.nodes[name] = Node(
-                            name,
-                            parent=self.nodes[parent],
-                            **n
-                        )
-                    except AssertionError:
-                        print(f"Malformed node representation: {n}")
-                    except KeyError:
-                        print(f"Detected a dangling node: {n['name']}")
-
-        except (KeyError):
-            raise Exception("Malformed input JSON string")
-
-
 ENVIRONMENT = os.environ.get('ENVIRONMENT', '')
 if ENVIRONMENT == 'google-cloud':
 
@@ -156,10 +37,8 @@ if ENVIRONMENT == 'google-cloud':
                 .get_bucket(os.environ['CREDENTIALS_BUCKET']) \
                 .get_blob(os.environ['CREDENTIALS_BLOB']) \
                 .download_as_string()
-    #parsed_vars = yaml.load(vars_blob, Loader=yaml.Loader)
     # https://stackoverflow.com/questions/6866600/how-to-parse-read-a-yaml-file-into-a-python-object
     TRELLIS = yaml.safe_load(config_doc)
-    #TRELLIS = Struct(**parsed_vars)
 
     PUBLISHER = pubsub.PublisherClient()
     STORAGE_CLIENT = storage.Client()
@@ -170,12 +49,12 @@ if ENVIRONMENT == 'google-cloud':
                         .get_blob(TRELLIS['LABEL_TAXONOMY']) \
                         .download_as_string()
 
-    TAXONOMY_PARSER = TaxonomyParser()
+    TAXONOMY_PARSER = trellis.TaxonomyParser()
     TAXONOMY_PARSER.read_from_string(label_taxonomy)
 else:
     import logging
 
-    TAXONOMY_PARSER = TaxonomyParser()
+    TAXONOMY_PARSER = trellis.TaxonomyParser()
     TAXONOMY_PARSER.read_from_json('label-taxonomy.json')
 
 
