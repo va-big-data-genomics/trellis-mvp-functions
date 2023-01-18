f5997dd15f671992199662fdc35aa6dbb062b6c5
Paul Billing-Ross <pbilling@stanford.edu>
2023-01-18 12:43:29 -0800

    Adding extract-vcf-regions files

---
+++ functions/extract-vcf-regions/main_test.py
@@ -0,0 +1,59 @@
+import json
+import mock
+import base64
+import google
+from uuid import uuid4
+import pytest
+
+from google.cloud import storage
+
+import main
+
+mock_context = mock.Mock()
+mock_context.event_id = '617187464135194'
+mock_context.timestamp = '2019-07-15T22:09:03.761Z'
+mock_context.resource = {
+                         'service': 'pubsub.googleapis.com', 
+                         'name': 'projects/my-gcp-project/topics/blob-update-storage-class', 
+                         'type': 'type.googleapis.com/google.pubsub.v1.PubsubMessage'
+}
+
+CLIENT = storage.Client()
+
+class TestMakeUniqueTestId:
+
+    def test_expected(self):
+        nodes = []
+        datetime_stamp = ""
+
+        task_id, trunc_nodes_hash = main.make_unique_task_id(nodes, datetime_stamp)
+        assert task_id ==
+        assert trunc_nodes_hash ==
+
+
+class TestMain:
+
+    def test_expected(self):
+        data = {
+                'body': {
+                         'cypher': 'MATCH (s:PersonalisSequencing)-[:GENERATED|WAS_USED_BY|LED_TO*]->(b:Blob) WHERE s.sample = "SHIP0" WITH COLLECT(DISTINCT(b)) AS all_blobs UNWIND all_blobs AS b MATCH p=(b)-[*1..2]-(:BiologicalOme) WHERE ALL (r in relationships(p) WHERE r.ontology="bioinformatics") WITH all_blobs, COLLECT(b) AS essential_blobs UNWIND all_blobs AS b MATCH (b) WHERE NOT b IN essential_blobs AND (NOT b.obj_exists = false OR NOT EXISTS(b.obj_exists)) AND b.bucket = "my-gcp-project-from-personalis-phase3-data" RETURN b.bucket AS bucket, b.path AS path ORDER BY b.size DESC LIMIT 100', 
+                         'results': {
+                                     'bucket': 'my-gcp-project-from-personalis', 
+                                     'path': 'va_mvp_phase2/DVALABP0/SHIP0/FASTQ/SHIP0_3_R2.fastq.gz',
+                                     'extension': 'fastq.gz',
+                                     'current_class': 'REGIONAL',
+                                     'requested_class': 'COLDLINE'
+                         }, 
+                },
+                'header': {
+                           'resource': 'queryResult',
+                           'method': 'VIEW',
+                           'dry-run': 'True'
+                }
+        }
+        data_str = json.dumps(data)
+        data_utf8 = data_str.encode('utf-8')
+        event = {'data': base64.b64encode(data_utf8)}
+
+        result = main.main(event, mock_context)
+        assert result == 2
